95f578734de547707d0f739ae303c160d889170c
amadeujunior1101 <amadeujunior00@gmail.com>
2021-02-26 16:03:17 -0300

    layout luby loteria

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "./")));
+
+app.post("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(3000, () => {
+  console.log("App listening on port 3000!");
+});
